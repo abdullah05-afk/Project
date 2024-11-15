a03be89870274e4836371fdb3ae377caa3172665
abdullah05-afk <abdullahmunir05@gmail.com>
2024-11-15 16:59:29 +0500

    container config done

--- Backend/server.js
+++ Backend/server.js
@@ -62,7 +62,7 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => console.log("MongoDB connected Successfully!"))
+.then(() => console.log("MongoDB is Connected Successfully!"))
 .catch(err => console.error("Could not connect to MongoDB:", err));
 
 // Start the server
